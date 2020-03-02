dac7635ba8d5ec1b175202a129d8b24a451f72fd
Gordan Blazic <gordanus@gmail.com>
2020-03-02 18:49:08 +0100

    Prepare for domain, SEO...

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,8 +14,8 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'gordanus@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Poruka sa sajta od strane:  $name";
+$to = 'info@kalkuset.rs'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Poruka sa sajta Kalkuset od strane:  $name";
 $email_body = "Stigla je nova poruka sa sajta Kalkuset.\n\n"."Detalji poruke:\n\nIme: $name\n\nEmail: $email_address\n\nTekst poruke:\n$message";
 $headers = "From: $email_address\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
